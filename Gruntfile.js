3f7ddded1984847af4048dde4fcd6881432fe81a
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-17 18:40:06 +0100

    Add grunt task to execute jasmine tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,18 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		coffee: {
+			compileForTest: {
+				options: {
+					bare: true
+				},
+				files: {
+					'tmp/Analyzer.js': ['src/Analyzer.coffee'],
+					'tmp/EventRouter.js': ['src/EventRouter.coffee'],
+					'tmp/Finger.js': ['src/Finger.coffee'],
+					'tmp/MethodsHelper.js': ['src/MethodsHelper.coffee'],
+					'tmp/StateMachine.js': ['src/StateMachine.coffee']
+				}
+			},
 			compileJoined: {
 				options: {
 					join: true
@@ -20,12 +32,23 @@ module.exports = function(grunt) {
 				src: 'build/<%= pkg.name %>.js',
 				dest: 'build/<%= pkg.name %>.min.js'
 			}
-		}
+		},
+		jasmine: {
+			src: 'tmp/*.js',
+			options: {
+				specs: 'spec/*Spec.js',
+				helpers: 'spec/*Helper.js'
+			}
+		},
+		clean: ['tmp']
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	// Default task(s).
-	grunt.registerTask('default', ['coffee', 'uglify']);
+	grunt.registerTask('default', ['coffee:compileJoined', 'uglify']);
+	grunt.registerTask('test', ['coffee:compileForTest', 'jasmine', 'clean']);
 };
