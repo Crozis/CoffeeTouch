3b94b51458beb77569470e0e903bd800ff3299c6
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-28 18:18:13 +0100

    Initial tests for FistTouch state

--- spec/StateMachineSpec.js
+++ spec/StateMachineSpec.js
@@ -65,6 +65,50 @@ describe("Automaton", function() {
 		});
 	});
 
+	describe("FirstTouch state", function() {
+		beforeEach(function () {
+			stateMachine	= new StateMachine(0, new EventRouter(element));
+			subject			= new FirstTouch(stateMachine);
+			stateMachine.currentState = subject;
+			fakeEvent		= {
+				touches: [0, 1]
+			};
+		});
+
+		it("is defined", function() {
+			expect(NoTouch).toBeDefined();
+		});
+		it("is a state", function() {
+			expect(subject).toBeAnInstanceOf(GenericState);
+		});
+		it("has a touchstart method", function() {
+			expect(subject.touchstart).toBeDefined();
+		});
+
+		describe("#touchstart", function() {
+			it("do not change the state of the automaton", function() {
+				$(subject).trigger('touchstart');
+				expect(stateMachine.currentState).toBeAnInstanceOf(FirstTouch);
+			});
+		});
+
+		describe("#touchmove", function() {
+			it("change the state of the automaton to FirstTouch state", function() {
+				stateMachine.router.grouper.analyser = { notify: function() {return}}
+				$(subject).trigger('touchmove');
+				expect(stateMachine.currentState).toBeAnInstanceOf(Drag);
+			});
+		});
+
+		describe("#touchend", function() {
+			it("do not change the state of the automaton", function() {
+				stateMachine.router.grouper.analyser = { notify: function() {return}}
+				$(subject).trigger('touchend');
+				expect(stateMachine.currentState).toBeAnInstanceOf(NoTouch);
+			});
+		});
+	});
+
 	describe("StateMachine", function() {
 		it("is defined", function() {
 			expect(StateMachine).toBeDefined();
