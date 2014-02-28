feb329ca6c3677925f8afba25783909a9d8ec5af
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-28 18:17:00 +0100

    Initial test for StateMachine

--- spec/StateMachineSpec.js
+++ spec/StateMachineSpec.js
@@ -1,5 +1,15 @@
-describe("StateMachine", function() {
-	it("is defined", function() {
-		expect(StateMachine).toBeDefined();
+describe("Automaton", function() {
+	var element, router, stateMachine, subject, fakeEvent;
+
+	beforeEach(function() {
+		loadFixtures('basic.html');
+		element			= $('div')[0];
+		router			= new EventRouter(element);
+		stateMachine	= new StateMachine(0, router);
+	});
+	describe("StateMachine", function() {
+		it("is defined", function() {
+			expect(StateMachine).toBeDefined();
+		});
 	});
 });
