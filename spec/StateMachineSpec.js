0da8cff0ced9d5c7d9b11e75afb02da64badd96a
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-28 18:17:45 +0100

    Initial tests for NoTouch state

--- spec/StateMachineSpec.js
+++ spec/StateMachineSpec.js
@@ -27,6 +27,44 @@ describe("Automaton", function() {
 			expect(subject.touchmove).toBeDefined();
 		});
 	});
+
+	describe("NoTouch state", function() {
+		beforeEach(function () {
+			stateMachine	= new StateMachine(0, new EventRouter(element));
+			subject			= new NoTouch(stateMachine);
+			fakeEvent		= {};
+		});
+
+		it("is defined", function() {
+			expect(NoTouch).toBeDefined();
+		});
+		it("is a state", function() {
+			expect(subject).toBeAnInstanceOf(GenericState);
+		});
+		it("has a touchstart method", function() {
+			expect(subject.touchstart).toBeDefined();
+		});
+
+		describe("#touchstart", function() {
+			it("change the state of the automaton to FirstTouch state", function() {
+				subject.touchstart(fakeEvent);
+				expect(stateMachine.currentState).toBeAnInstanceOf(FirstTouch);
+			});
+		});
+		describe("#touchmove", function() {
+			it("do not change the state of the automaton", function() {
+				subject.touchmove(fakeEvent);
+				expect(stateMachine.currentState).toBeAnInstanceOf(NoTouch);
+			});
+		});
+		describe("#touchend", function() {
+			it("do not change the state of the automaton", function() {
+				subject.touchend(fakeEvent);
+				expect(stateMachine.currentState).toBeAnInstanceOf(NoTouch);
+			});
+		});
+	});
+
 	describe("StateMachine", function() {
 		it("is defined", function() {
 			expect(StateMachine).toBeDefined();
