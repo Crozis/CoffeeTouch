e46a340a472e914ff2413b5af6982b1a88009ecd
Nicolas Dupont <npg.dupont@gmail.com>
2014-02-28 18:17:16 +0100

    Initial tests for GenericState

--- spec/StateMachineSpec.js
+++ spec/StateMachineSpec.js
@@ -7,6 +7,26 @@ describe("Automaton", function() {
 		router			= new EventRouter(element);
 		stateMachine	= new StateMachine(0, router);
 	});
+
+	describe("GenericState", function() {
+		beforeEach(function() {
+			subject = new GenericState(stateMachine);
+		});
+
+		it("is defined", function() {
+			expect(GenericState).toBeDefined();
+		});
+
+		it("has a touchstart method", function() {
+			expect(subject.touchstart).toBeDefined();
+		});
+		it("has a touchend method", function() {
+			expect(subject.touchend).toBeDefined();
+		});
+		it("has a touchmove method", function() {
+			expect(subject.touchmove).toBeDefined();
+		});
+	});
 	describe("StateMachine", function() {
 		it("is defined", function() {
 			expect(StateMachine).toBeDefined();
